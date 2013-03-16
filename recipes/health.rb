328ee07a45a2767557507468e08f92dabc0becc5
Maximilian Rüdiger <m.ruediger@alice-dsl.net>
2013-03-16 23:41:45 +0100

    fixed options for runit

--- recipes/health.rb
+++ recipes/health.rb
@@ -14,15 +14,17 @@ end
 unless Chef::Config[:solo]
   puts '###### ---- Using Chef Server Mode for Riemann ---- ######'
   runit_service "riemann-health" do
-    options :riemann_host => search(:node, "recipe:riemann\\:\\:server AND chef_environment:#{node.chef_environment}").first
-    options :name => node.name
+    options ({
+    :riemann_host => search(:node, "recipe:riemann\\:\\:server AND chef_environment:#{node.chef_environment}").first,
+    :name => node.name}.merge(params))
   end
 else
   puts '###### ---- Using Chef Solo Mode for Riemann ---- ######'
   runit_service "riemann-health" do
     riemann_server = search(:riemann, "id:riemann_server").first
-    options :riemann_host => riemann_server['ipaddress']
-    options :name => node[:network][:interfaces][:eth1][:addresses].keys.first
+    options ({
+      :riemann_host => riemann_server['ipaddress'],
+      :name => node[:network][:interfaces][:eth1][:addresses].keys.first}.merge(params))
   end
 end
 
