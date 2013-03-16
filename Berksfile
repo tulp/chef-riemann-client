f6dcd6d8fdd5c19866c3f91b8f1569fc6d364ca3
Maximilian Rüdiger <m.ruediger@alice-dsl.net>
2013-03-16 21:34:15 +0100

    last part of moving riemann-client into own cookbook

--- Berksfile
+++ Berksfile
@@ -2,3 +2,4 @@ site :opscode
 
 metadata
 cookbook 'runit'
+#cookbook 'chef-solo-search', git: 'git://github.com/edelight/chef-solo-search.git'
--- recipes/health.rb
+++ recipes/health.rb
@@ -1,20 +1,29 @@
 #riemann_server = 
 # we currently dont use chef server so we have do define the server-ip ourselve
-riemann_server = '192.168.90.100'
+
+service 'riemann-health' do
+  supports :restart => true
+end
+
+gem_package "riemann-tools" do
+  version '0.1.1'
+  action :install
+  notifies :restart, resources(:service => 'riemann-health')
+end
 
 unless Chef::Config[:solo]
+  puts '###### ---- Using Chef Server Mode for Riemann ---- ######'
   runit_service "riemann-health" do
     options :riemann_host => search(:node, "recipe:riemann\\:\\:server AND chef_environment:#{node.chef_environment}").first
     options :name => node.name
   end
 else
+  puts '###### ---- Using Chef Solo Mode for Riemann ---- ######'
   runit_service "riemann-health" do
-    options :riemann_host =>
-    options :name => 
+    riemann_server = search(:riemann, "id:riemann_server").first
+    options :riemann_host => riemann_server['ipaddress']
+    options :name => node[:network][:interfaces][:eth1][:addresses].keys.first
   end
 end
 
-service 'riemann-health' do
-  supports :restart => true
-  action [:start]
-end
+
