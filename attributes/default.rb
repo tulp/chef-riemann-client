d0f9c9689f50a2754fb79533726a9425fa2d8215
Maximilian Rüdiger <m.ruediger@alice-dsl.net>
2013-03-19 20:50:32 +0100

    added fixed_ip check

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,8 +5,13 @@ default[:riemann][:ruby_exec] = "/opt/chef/embedded/bin/ruby"
 #general runner
 default[:riemann][:riemann_runner_executable] = "/usr/bin/riemann-runner"
 
-#nova-check specific runner
-default[:riemann][:nova][:app_name] = "nova-check"
+#nova-check specific
 default[:riemann][:nova][:interval] = 60
 default[:riemann][:nova][:riemann_runner_executable] = "/usr/bin/riemann-runner-nova"
 default[:riemann][:nova][:riemann_executable] = "/usr/bin/riemann-nova-service"
+
+#fixed-ip check
+default[:riemann][:fixed_ip][:threshold] = 2
+default[:riemann][:fixed_ip][:interval] = 60
+default[:riemann][:fixed_ip][:riemann_runner_executable] = "/usr/bin/riemman-runner-fixed"
+default[:riemann][:fixed_ip][:riemann_executable] = "/usr/bin/riemann-fixed-service"
