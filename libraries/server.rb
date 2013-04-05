28c6d02d287942869587c2e4fb9b86fb0d2df7b1
Thomas Kadauke <tkadauke@imedo.de>
2013-04-05 09:35:14 +0200

    Chef search in production seems to be broken. This query works for some reason.

--- libraries/server.rb
+++ libraries/server.rb
@@ -2,6 +2,6 @@ def get_riemann_server_ip
   if node[:riemann][:server][:bind_ip]
     node[:riemann][:server][:bind_ip]
   else
-    search(:node, "riemann_server_service:true AND chef_environment:#{node.chef_environment}").map {|n| n[:riemann][:server][:bind_ip]}.first
+    search(:node, "recipes:riemann-server AND chef_environment:#{node.chef_environment}").map {|n| n[:riemann][:server][:bind_ip]}.first
   end
 end
